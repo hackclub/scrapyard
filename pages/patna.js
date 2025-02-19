ef79241781b18c5700e2585ab9ae131d453ca2f4
Pulkit Parmar <pulkitparmar09@gmail.com>
2025-02-19 06:30:07 +0530

    Updated Scrapyard Patna event page with full event details

---
+++ pages/patna.js
@@ -0,0 +1,19 @@
+import { Scrapyard } from "../components/Scrapyard";
+
+export default function Patna() {
+  return (
+    <Scrapyard
+      name="Scrapyard Patna"
+      location="Patna, India (Exact location TBA)"
+      date="March 16, 2025"
+      time="8 AM - 8 PM (12 hours)"
+      description="Scrapyard Patna is a 12-hour hackathon where participants will build innovative projects using scrap materials."
+      signupLink="https://forms.hackclub.com/scrapyard-signup?event=patna"
+      aboutEvent="Scrapyard Patna is a fun and creative hackathon where participants will build projects using old and recycled materials. We’ll provide food, drinks, and an amazing environment for making awesome things!"
+      participants="Open registration (No fixed limit yet)"
+      food="Provided (Breakfast, Lunch, Dinner, Snacks, Water)"
+      contactEmail="pulkitparmar09@gmail.com"
+      slackChannel="https://hackclub.slack.com/archives/C08AGCCLMJL"
+    />
+  );
+}
